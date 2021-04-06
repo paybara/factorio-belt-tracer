306c59112c70159a391307e3e727dd0aa876961b
paybara <81732769+paybara@users.noreply.github.com>
2021-04-06 09:37:48 -0700

    Trace pipes too

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Belt Tracer mod for [factorio](https://www.factorio.com/)
 
-This is a simple mod for factorio to trace a belt through your spaghetti. Just hover over a belt and press the hotkey (default: `SHIFT-H`). Press it somewhere else to clear the trace.
+This is a simple mod for factorio to trace a belt through your spaghetti. Just hover over a belt (or pipe!) and press the hotkey (default: `SHIFT-H`). Press it somewhere else to clear the trace.
 
 Only the belts *relevant to the selected belt* are traced. Meaning only belts whose items might pass through the belt you selected, or might have passed through it, will be traced. For example, it won't trace a branch upstream that went in a different direction.
 
@@ -12,13 +12,14 @@ Only the belts *relevant to the selected belt* are traced. Meaning only belts wh
 
 ## Known/suspected issues
 * I suspect if you highlight a belt, save, disable the mod and then load ...then the highlight will be permanently affixed to the map (until the belts are destroyed).
-* I've not attempted to make this scalable, it currently traces the whole belt in the handler. It traces O(1000) length belts without a hiccup but I haven't tried it on a megabase. 
+* I've not attempted to make this scalable, it currently traces the whole belt in the handler. It traces O(1000) length belts without a hiccup but I haven't tried it on a megabase.
+* When tracing pipes to boilers, both water and steam will be traced.
+* Other fluid-carrying entities (e.g. from mods) are unlikely to work.
 
 ## Potential improvements
 * Allow multiple highlights.
 * Trace both sides of the belt separately.
 * Figure out how to show the trace on the map.
-* Trace pipes.
 * Better colors.
 * GUI, e.g. an extra panel when you click on a belt, with a button to trace it and maybe some other info. (Like it should be able to list the current belt content, if that'd be useful.)
 
--- control.lua
+++ control.lua
@@ -13,9 +13,19 @@ local function is_transport_line(e)
     return false  
 end
 
+local function is_pipe(e)
+    local pipe_types = {'pipe', 'pipe-to-ground', 'storage-tank', 'pump', 'offshore-pump', 'generator', 'boiler'}
+    for _,pipe_type in pairs(pipe_types) do
+        if e.type == pipe_type then
+            return true
+        end
+    end
+    return false  
+end
+
 local global_prefix = "paybara-belt-tracer-"
 local function global_ids(p) 
-    return global_prefix..p.name"-ids"
+    return global_prefix..p.name.."-ids"
 end
 
 -- Draw a line between two entities, on surface s, visible to player p.
@@ -45,31 +55,8 @@ local function draw_line(from, to, s, p, dashed)
     -- attaching them directly to the entities on the map? I should check what bottleneck does...
 end
 
--- This is the main handler function, it runs whenever the trace action is triggered.
-script.on_event('paybara:trace-belt', function(event)
-    local p = game.players[event.player_index]
-
-    -- Remove any previous trace first, regardless of what you're hovering over.
-    if global[(global_ids(p))] ~= nil then
-        for id, _ in pairs(global[global_ids(p)]) do
-            rendering.destroy(id)
-        end
-    end
-    global[global_ids(p)] = {}
-
-    local e = p.selected
-    if not e then
-        -- p.print('not over anything')
-        return
-    end
-    if not is_transport_line(e) then
-        -- p.print('not a belt. name: '..e.name..' type: '..e.type)
-        return
-    end
-
-    local s = e.surface
-
-    -- Leave a circle at the selected tile so you can see where you traced from.
+-- Draw a small circle on entitie e, on surface s, visible to player p.
+local function draw_circle(e, s, p)
     local id = rendering.draw_circle({
         ["target"]=e,
         ["radius"]=.3,
@@ -82,11 +69,18 @@ script.on_event('paybara:trace-belt', function(event)
     })
     -- Save the ID in the global table so it can be cleaned up later.
     global[global_ids(p)][id] = true
- 
-    -- The string key for the table of finished belts.
-    local function finishkey(entity)
-        return position_to_string(entity.position)
-    end
+end
+
+-- The string key for a table of finished belts or pipes.
+local function finishkey(entity)
+    return position_to_string(entity.position)
+end
+
+local function trace_belt(p, e)
+    local s = e.surface
+
+    -- Leave a circle at the selected tile so you can see where you traced from.
+    draw_circle(e, s, p)
 
     local num_entities = 1
     local num_steps = 1
@@ -107,8 +101,7 @@ script.on_event('paybara:trace-belt', function(event)
             keep_going = false
             for _, e in pairs(to_be_walked) do
                 -- Get the belts that are connected to this one as inputs or outputs (depending on which pass we're doing).
-                local ns = e.belt_neighbours[in_out]
-                for _, n in pairs(ns) do
+                for _, n in pairs(e.belt_neighbours[in_out]) do
                     -- p.print(in_out.." belt_neighbor "..n.name.." at "..position_to_string(n.position))
                     draw_line(e, n, s, p, false)
                     if finished[finishkey(n)] == nil then
@@ -148,4 +141,96 @@ script.on_event('paybara:trace-belt', function(event)
     -- TODO: Put the print statements behind a verbose setting.
     -- p.print("Traced "..num_entities.." belts in "..num_steps.." steps.")
     -- TODO: Play a sound when the trace completes?
-end) 
+end
+
+-- Traces pipes and other entities that hold fluid by walking their 'neighbours'
+--
+-- TODO: Consider only walking fluid boxes that have a matching locked fluid.
+-- For instance, this will avoid jumping between steam and water in boilers.
+-- This might let us relax the extra is_pipe check on recursion: if we can save the input fluid box/index,
+-- we can look for any other fluid boxes with the same fluid and keep walking them.
+-- This should let us expand this to mods that let fluids flow through other entities, like Industrial Revolution.
+-- But done naively it might make us jump between systems that are actually disconnected,
+-- if they're connected to separate ports on entites that allow multiple inputs/outputs but don't flow between them.
+-- I haven't found how the game distinguishes between these, but it does - boilers and steam engines have an icon for bi-di flow,
+-- and e.g. chemical plants have icons for only input and only output.
+local function trace_pipe(p, e) 
+    local s = e.surface
+
+    -- Leave a circle at the selected tile so you can see where you traced from.
+    draw_circle(e, s, p)
+
+    local num_entities = 1
+    local num_steps = 1
+    
+    local to_be_walked = {e}
+    local finished = {[finishkey(e)]=true}
+    local keep_going = true
+    while keep_going do
+        local next_pass = {}
+        local num_next = 0
+        keep_going = false
+        for _, e in pairs(to_be_walked) do
+            -- For belt-connected entities, neighbors is "an array of entity arrays of all entities a given fluidbox is connected to."
+            for _, ns in pairs(e.neighbours) do
+                for _, n in pairs(ns) do
+                    local dashed = false
+                    if e.type == "pipe-to-ground" and n.type == "pipe-to-ground" then
+                        dashed = true
+                    end
+                    if finished[finishkey(n)] == nil or not dashed then
+                        -- Draw lines even if we've already visited entities to fill in grids of pipes or tanks.
+                        -- Just don't double-draw dashed lines, as that can mess them up.
+                        draw_line(e, n, s, p, dashed)
+                    end
+                    if finished[finishkey(n)] == nil then
+                        -- Pipes can be connected to non-pipes, like assemblers. Draw lines to assemblers but don't recurse into them.
+                        if is_pipe(n) then
+                            num_next = num_next + 1
+                            next_pass[num_next] = n
+                            finished[finishkey(n)] = true
+                        end
+                    end                    
+                end
+            end
+        end
+        if num_next > 0 then
+            to_be_walked = next_pass
+            keep_going = true
+            num_steps = num_steps + 1
+            num_entities = num_entities + num_next
+            p.print("Step "..num_steps.." found "..num_next.." new belts.")
+        else
+            p.print("Finished in "..num_steps.." steps.")
+        end
+    end
+end
+
+
+-- This is the main handler function, it runs whenever the trace action is triggered.
+script.on_event('paybara:trace-belt', function(event)
+    local p = game.players[event.player_index]
+
+    -- Remove any previous trace first, regardless of what you're hovering over.
+    if global[(global_ids(p))] ~= nil then
+        for id, _ in pairs(global[global_ids(p)]) do
+            rendering.destroy(id)
+        end
+    end
+    global[global_ids(p)] = {}
+
+    local e = p.selected
+    if not e then
+        -- p.print('not over anything')
+        return
+    end
+    if is_transport_line(e) then
+        trace_belt(p, e)
+        return
+    end
+    if is_pipe(e) then
+        trace_pipe(p, e)
+        return
+    end
+    p.print('not a belt. name: '..e.name..' type: '..e.type)
+end)
