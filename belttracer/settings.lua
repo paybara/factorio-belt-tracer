d821589666fd63368f163033294a8604f43700cb
paybara <ashamanm@gmail.com>
2023-04-04 16:41:19 +1000

    Add a setting for line thickness and increase default thickness to 3

--- belttracer/settings.lua
+++ belttracer/settings.lua
@@ -4,5 +4,11 @@ data:extend({
         name = "belttracer-verbose-logging",
         setting_type = "runtime-per-user",
         default_value = false
+    },
+    {
+        type = "int-setting",
+        name = "belttracer-line-thickness",
+        setting_type = "runtime-per-user",
+        default_value = 3,
     }
 })
